016a43d5d039e3e90877a84cfe6d2a8af56a99fc
LFueyo <lucas.fueyo@epsi.fr>
2014-03-05 16:37:38 +0100

    Main.c squelette

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+typedef struct Note Note;
+struct Note
+{
+
+};
+
+typedef struct Accord Accord;
+struct Accord
+{
+
+};
+
+typedef struct Musique Musique;
+struct Musique
+{
+
+};
+
+int main(int argc, char *argv[])
+{
+
+}
