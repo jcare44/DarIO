a8c69ed5f1cdeb26045523f3bc4fab815258136c
erac44 <ilovedecone44@hotmail.fr>
2014-03-19 09:57:46 +0100

    Début de définitions des structures et de leurs bornes

--- main.c
+++ main.c
@@ -3,25 +3,42 @@
 #include <time.h>
 #include <math.h>
 
+#define MIDI_PAUSE 0 //La valeur midi de la pause
+#define MIDI_MIN 36
+#define MIDI_MAX 95
+#define TIME {0.25,0.5,1,2,4} //De la double croche à la ronde
+#define BAR_MIN 100
+#define BAR_MAX 100
+#define TEMPO_MIN 120
+#define TEMPO_MAX 120
+
 typedef struct Note Note;
+typedef struct Chord Chord;
+typedef struct Piece Piece;
+
 struct Note
 {
-
+	int midi;
+	float time;
 };
 
-typedef struct Accord Accord;
-struct Accord
+struct Chord
 {
-
+	int numberOfNotes;
+	Note notes[];
 };
 
-typedef struct Musique Musique;
-struct Musique
+struct Piece
 {
-
+	int bar;
+	float tempo;
+	int numberOfChords;
+	Chord chords[];
 };
 
 int main(int argc, char *argv[])
 {
-
+	
+	
+	return 0;
 }
