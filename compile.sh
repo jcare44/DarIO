a3f657ead675690c9b65b674797893f64d0993e5
martin cailleau <martin.cailleau@gmail.com>
2014-05-29 11:51:13 +0200

    communication in osc with pure data

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 
 rm -Rf src/*/*.gch
 rm -Rf src/*/*.o
-g++ -std=c++0x src/main.cpp src/abstracts/* src/models/* src/constraints/* config/*
+g++ -std=c++0x src/main.cpp src/abstracts/* src/models/* src/constraints/* config/* src/osc/* src/ip/*
 rm -Rf src/*/*.gch
 rm -Rf src/*/*.o
